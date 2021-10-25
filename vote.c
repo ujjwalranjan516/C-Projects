c371d6cf6dcc237a1edbf9c00bbd37a3cccd8c0b
ujjwalranjan516 <82654014+ujjwalranjan516@users.noreply.github.com>
2021-10-25 22:31:46 +0530

    Add files via upload

---
+++ vote.c
@@ -0,0 +1,17 @@
+//program to find a person is vote or not
+#include<stdio.h>
+int main ()
+{
+    int age;
+    printf("ebter age");
+    scanf("%d",&age);
+    if(age>18)
+    {
+    	printf("eligible to vote");
+	}
+	else
+	{
+		printf("not eligible to vote");
+	}
+
+}
